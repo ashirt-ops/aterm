e7cac43874cca3d7e7d00d35f66d3608cf6b5ab3
Joel Smith <Hikash@gmail.com>
2020-08-25 19:09:25 -0700

    add TestConnection func

--- network/get_operations.go
+++ network/get_operations.go
@@ -4,6 +4,7 @@
 package network
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/theparanoids/ashirt-server/backend/dtos"
@@ -28,4 +29,31 @@ func GetOperations() ([]dtos.Operation, error) {
 	err = readResponseBody(&ops, resp.Body)
 
 	return ops, err
+}
+
+var ErrorConnectionUnauthorized = errors.New("Could not connect: Unauthorized")
+var ErrorConnectionNotFound = errors.New("Could not connect: Not Found")
+var ErrorConnectionUnknownStatus = errors.New("Could not connect: Unknown status")
+
+// TestConnection performs a basic query to the backend and interprets the results.
+// There are a few scenarios. A successful connection returns ("", nil)
+// Otherwise, the return structure is ("suggestion to fix (if any)", underlyingError)
+// the underlying error is likely (but not necessarily) one of:
+// ErrorConnectionUnknownStatus, ErrorConnectionNotFound, ErrorConnectionUnauthorized
+// use errors.Is(err, target) to check these errors
+func TestConnection() (string, error) {
+	resp, err := makeJSONRequest("GET", apiURL+"/operations", http.NoBody)
+	if err != nil {
+		return "", err
+	}
+	statusCode := resp.StatusCode
+	if statusCode == http.StatusOK {
+		return "", nil
+	} else if statusCode == http.StatusUnauthorized {
+		return "Check API and Secret keys", ErrorConnectionUnauthorized
+	} else if statusCode == http.StatusNotFound {
+		return "Check API URL", ErrorConnectionNotFound
+	} else {
+		return "", fmt.Errorf("%w : Status Code: %v", ErrorConnectionUnknownStatus, statusCode)
+	}
 }
