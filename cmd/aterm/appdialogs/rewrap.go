8cc45dfa487e4d86696ac3fe6c0ee9175f0f5e21
Joel Smith <Hikash@gmail.com>
2020-09-15 16:44:04 -0700

    provide wrappers for selct and user queries

--- cmd/aterm/appdialogs/rewrap.go
+++ cmd/aterm/appdialogs/rewrap.go
@@ -7,9 +7,19 @@ func UserQuery(question string, defaultValue *string) (string, error) {
 	return dialog.UserQuery(question, defaultValue, internalMenuState.DialogInput)
 }
 
+// HandleUserQuery is a re-packaging of dialog.HandleUserQuery with inputStream pre-provided
+func HandleUserQuery(question string, defaultValue *string, bailFunc func()) dialog.QueryResponse {
+	return dialog.HandleUserQuery(question, defaultValue, internalMenuState.DialogInput, bailFunc)
+}
+
 // PlainSelect is a re-packaing of the dialog.PlainSelect with inputStream pre-provided
 func PlainSelect(label string, options []dialog.SimpleOption) (dialog.SimpleOption, error) {
 	return dialog.PlainSelect(label, options, internalMenuState.DialogInput)
+}
+
+// HandlePlainSelect is a re-packaging of dialog.HandlePlainSelect with inputStream pre-provided
+func HandlePlainSelect(label string, options []dialog.SimpleOption, bailFunc func() dialog.SimpleOption) dialog.SelectResponse {
+	return dialog.HandlePlainSelect(label, options, internalMenuState.DialogInput, bailFunc)
 }
 
 // YesNoSelect is a re-packaing of the dialog.YesNoPrompt with inputStream pre-provided
--- dialog/options.go
+++ dialog/options.go
@@ -3,7 +3,12 @@
 
 package dialog
 
-import "io"
+import (
+	"errors"
+	"io"
+
+	"github.com/manifoldco/promptui"
+)
 
 // InvalidSelection is a predefined selection representing choices that errored out.
 var InvalidSelection = SimpleOption{invalid: true}
@@ -45,4 +50,67 @@ func PlainSelect(label string, options []SimpleOption, inputStream io.ReadCloser
 		return InvalidSelection, err
 	}
 	return options[selectedItem], nil
+}
+
+// SelectResponse captures the net result from a Select Menu selection. See HandlePlainSelect for
+// more details. Also note that this struture is mirrored by QueryResponse, for free text entry.
+type SelectResponse struct {
+
+	// Selection represents the specfic selection that was made by a user in the select menu.
+	Selection SimpleOption
+
+	// Action represents how the user interacted with the select menu -- specifically, did they enter
+	// a value, or did they try to avoid entering value, or did the select menu generate an error
+	Action UserAction
+
+	// Err is the actual error encountered by the user, if any. Note that the underlying system treats
+	// ^c and ^d as errors, whereas these errors are instead translated into actions for
+	// this system, and are not represented as errors here.
+	Err error
+}
+
+// IsKillSignal checks to see if the user action was either a UserActionExit or UserActionCancel --
+// i.e. the user tried to back out of the interaction point
+func (resp *SelectResponse) IsKillSignal() bool {
+	return resp.Action == UserActionExit || resp.Action == UserActionCancel
+}
+
+// UserAction is an effective enum representing the various states a user can get into when prompted
+// for a response (referred to in documentation as an "interaction point")
+type UserAction string
+
+const (
+	// UserActionCancel represents a user pressing ^c (Interrupt) when given an interaction point
+	UserActionCancel UserAction = "cancel"
+	// UserActionExit represents a user pressing ^d (EOF) when given an interaction point
+	UserActionExit UserAction = "exit"
+	// UserActionEntered represents a user providing a value when given an interaction point (i.e. the normal expectation)
+	UserActionEntered UserAction = ""
+	// UserActionErrored represents a user encountering an error during an interaction point
+	UserActionErrored UserAction = "error"
+)
+
+// HandlePlainSelect provides a small wrapper around PlainSelect. This function will generate a CLI
+// Select menu, then interpret the results to check if some error was encountered, or if the user
+// pressed ^d or ^c to exit out of the select menu. If so, bailFunc is executed, and the caller may
+// supply a replacement value in these sencarios. This data is all captured, then returned in a
+// SelectResponse, which can be pulled apart to retrieve the selection or error as before
+func HandlePlainSelect(label string, options []SimpleOption, inputStream io.ReadCloser, bailFunc func() SimpleOption) SelectResponse {
+	selection, err := PlainSelect(label, options, inputStream)
+	var resp SelectResponse
+
+	if errors.Is(err, promptui.ErrInterrupt) {
+		resp = SelectResponse{Selection: selection, Action: UserActionCancel}
+	} else if errors.Is(err, promptui.ErrEOF) {
+		resp = SelectResponse{Selection: selection, Action: UserActionExit}
+	} else if err != nil {
+		resp = SelectResponse{Selection: selection, Action: UserActionErrored, Err: err}
+	} else {
+		resp = SelectResponse{Selection: selection}
+	}
+	if resp.IsKillSignal() {
+		resp.Selection = bailFunc()
+	}
+
+	return resp
 }
