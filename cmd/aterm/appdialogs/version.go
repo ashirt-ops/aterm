77fbfe6ea94d2b5fbd052ed0810c20bdfce39842
Joel Smith <joel.smith@originate.com>
2020-09-28 11:18:15 -0700

    add option for extended version (#16)

--- cmd/aterm/appdialogs/version.go
+++ cmd/aterm/appdialogs/version.go
@@ -7,5 +7,11 @@ import (
 // PrintVersion prints a simple sentence that lists the current version and commit hash
 // Note: this should only be called _after_ parsing CLI options
 func PrintVersion() {
-	printfln("ATerm Version: %v  Build Hash: %v", config.Version(), config.CommitHash())
+	printline("ATerm Version:", config.Version(), " Build Hash:", config.CommitHash())
+}
+
+// PrintExtendedVersion prints more version information: the go runtime and the build date
+// Note: this should only be called _after_ parsing CLI options
+func PrintExtendedVersion() {
+	printline("Go runtime:", config.GoRuntime(), " Build Date:", config.BuildDate())
 }
